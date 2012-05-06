2ded57ac9bd9bd70a0b8fff50aa71877d72f4da5
Andrey Esaulov <esaulov.andrey@gmail.com>
2012-05-06 23:20:30 +0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Microsample::Application.routes.draw do
   
+  get "users/new"
+
   root                        to: "static_pages#home" 
+  
+  match '/signup',            to: "users#new"
   
   match '/help',              to: "static_pages#help"
   match '/contact',           to: "static_pages#contact"
